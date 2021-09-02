8bf57686a88ef65636493b00d4ea36927db742b3
mbm6415 <audrius.budrys@teliacompany.com>
2021-09-02 08:58:03 +0300

    Added "multiply by 5" and "divide by 3 remainder" tests

--- LinqSelectTest/SelectorTests.cs
+++ LinqSelectTest/SelectorTests.cs
@@ -15,8 +15,8 @@ namespace LinqSelectTest
 
             //Act
             var numbersAddOne = numbers.Transform(TransformNum);
-            //Arrange
             var convertedNumbers = new List<int> { 2, 3, 4 };
+
             //Assert
             Assert.Equal(convertedNumbers, numbersAddOne);
 
@@ -34,11 +34,57 @@ namespace LinqSelectTest
             var numbers = new int[] { 1, 2, 3 };
 
             //Act
-            var numbersAddOne = numbers.AddsOne();
+            var numbersAddOne = numbers.Transform(TransformNum);
             var convertedNumbers = new int[] { 2, 3, 4 };
+
             //Assert
             Assert.Equal(convertedNumbers, numbersAddOne);
 
+            int TransformNum(int num)  //veliau galima sukurt anonimus, pakolkas pasizaist su debugeriu
+            {
+                int transformedNumber = num + 1;
+                return transformedNumber;
+            }
+        }
+
+        [Fact]
+        public void Multiply_Each_Array_Item_By_5()
+        {
+            //Arrange
+            var numbers = new int[] { 1, 2, 3 };
+
+            //Act
+            var multipliedNumbers = numbers.Transform(TransformNum);
+            var convertedNumbers = new int[] { 5, 10, 15 };
+
+            //Assert
+            Assert.Equal(convertedNumbers, multipliedNumbers);
+
+            int TransformNum(int num)
+            {
+                int transformedNumber = num * 5;
+                return transformedNumber;
+            }
+        }
+
+        [Fact]
+        public void Find_Division_By_3_Remainder_Of_Each_Array_Item()
+        {
+            //Arrange
+            var numbers = new int[] { 2, 3, 4 };
+
+            //Act
+            var numberRemainders = numbers.Transform(TransformNum);
+            var convertedNumbers = new int[] { 2, 0, 1 };
+
+            //Assert
+            Assert.Equal(convertedNumbers, numberRemainders);
+
+            int TransformNum(int num)  //veliau galima sukurt anonimus, pakolkas pasizaist su debugeriu
+            {
+                int transformedNumber = num % 3;
+                return transformedNumber;
+            }
         }
     }
 }
