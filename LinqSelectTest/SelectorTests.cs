432147e6f37caeabbf61dcd473746d1c905821f3
mbm6415 <audrius.budrys@teliacompany.com>
2021-09-03 08:41:49 +0300

    Changed methods to lambda expressions in SelectorTests class

--- LinqSelectTest/SelectorTests.cs
+++ LinqSelectTest/SelectorTests.cs
@@ -14,17 +14,17 @@ namespace LinqSelectTest
             var numbers = new List<int> { 1, 2, 3 };
 
             //Act
-            var numbersAddOne = numbers.Transform(TransformNum);
+            var numbersAddOne = numbers.Transform(num => num + 1);
             var convertedNumbers = new List<int> { 2, 3, 4 };
 
             //Assert
             Assert.Equal(convertedNumbers, numbersAddOne);
 
-            int TransformNum(int num)  //veliau galima sukurt anonimus, pakolkas pasizaist su debugeriu
-            {
-                int transformedNumber = num + 1;
-                return transformedNumber;
-            }
+            //int TransformNum(int num)  //veliau galima sukurt anonimus, pakolkas pasizaist su debugeriu
+            //{
+            //    int transformedNumber = num + 1;
+            //    return transformedNumber;
+            //}
         }
 
         [Fact]
@@ -34,17 +34,11 @@ namespace LinqSelectTest
             var numbers = new int[] { 1, 2, 3 };
 
             //Act
-            var numbersAddOne = numbers.Transform(TransformNum);
+            var numbersAddOne = numbers.Transform(num => num + 1);
             var convertedNumbers = new int[] { 2, 3, 4 };
 
             //Assert
             Assert.Equal(convertedNumbers, numbersAddOne);
-
-            int TransformNum(int num)  //veliau galima sukurt anonimus, pakolkas pasizaist su debugeriu
-            {
-                int transformedNumber = num + 1;
-                return transformedNumber;
-            }
         }
 
         [Fact]
@@ -54,17 +48,11 @@ namespace LinqSelectTest
             var numbers = new int[] { 1, 2, 3 };
 
             //Act
-            var multipliedNumbers = numbers.Transform(TransformNum);
+            var multipliedNumbers = numbers.Transform(num => num * 5);
             var convertedNumbers = new int[] { 5, 10, 15 };
 
             //Assert
             Assert.Equal(convertedNumbers, multipliedNumbers);
-
-            int TransformNum(int num)
-            {
-                int transformedNumber = num * 5;
-                return transformedNumber;
-            }
         }
 
         [Fact]
@@ -74,17 +62,11 @@ namespace LinqSelectTest
             var numbers = new int[] { 2, 3, 4 };
 
             //Act
-            var numberRemainders = numbers.Transform(TransformNum);
+            var numberRemainders = numbers.Transform(num => num % 3); 
             var convertedNumbers = new int[] { 2, 0, 1 };
 
             //Assert
             Assert.Equal(convertedNumbers, numberRemainders);
-
-            int TransformNum(int num)  //veliau galima sukurt anonimus, pakolkas pasizaist su debugeriu
-            {
-                int transformedNumber = num % 3;
-                return transformedNumber;
-            }
         }
     }
 }
